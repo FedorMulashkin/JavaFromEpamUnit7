67e5939d999cb7f2085707768aa0ec5cdf572446
Fedor Mulashkin <fedor.mulashkin@gmail.com>
2022-05-09 12:18:12 +0300

    Add Anagram class

---
+++ src/task/Anagram.java
@@ -0,0 +1,33 @@
+package task;
+
+
+import java.util.*;
+
+public class Anagram {
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        } else if (s1.length() != s2.length()) {
+            return false;
+        }
+        List<Character> charS1 = makeCharList(s1);
+        List<Character> charS2 = makeCharList(s2);
+        for (Character i:charS1) {
+            if (charS2.contains(i)){
+                charS2.remove(i);
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<Character> makeCharList(String string) {
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            list.add(string.charAt(i));
+        }
+        return list;
+    }
+}
---
+++ src/task/MainClass.java
@@ -0,0 +1,9 @@
+package task;
+
+public class MainClass {
+    public static void main(String[] args) {
+        String s1 = "qwertyqq";
+        String s2 = "qqqwerty";
+        System.out.println(Anagram.isAnagram(s1, s2));
+    }
+}
